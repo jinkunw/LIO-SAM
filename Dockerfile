7627a6e7e11970225aebe77a4b3d2d449df41df8
Jinkun Wang <jinkwang@amazon.com>
2022-07-24 14:26:02 -0600

    ouster tunning

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ RUN apt-get update \
     && apt-get install -y ros-noetic-navigation \
     && apt-get install -y ros-noetic-robot-localization \
     && apt-get install -y ros-noetic-robot-state-publisher \
+    && apt-get install -y ros-noetic-imu-complementary-filter \
     && rm -rf /var/lib/apt/lists/*
 
 RUN apt-get update \
@@ -16,19 +17,21 @@ RUN apt-get update \
 
 SHELL ["/bin/bash", "-c"]
 
-# LIO-SAM
+# OUSTER
 RUN mkdir -p /root/catkin_ws/src \
     && cd /root/catkin_ws/src \
-    && git clone https://github.com/jinkunw/LIO-SAM.git \
-    && cd .. \
-    && source /opt/ros/noetic/setup.bash \
-    && catkin_make -DCMAKE_BUILD_TYPE=Release
-
-# OUSTER
-RUN cd /root/catkin_ws/src \
     && source /opt/ros/noetic/setup.bash \
     && git clone https://github.com/ouster-lidar/ouster_example.git \
     && cd .. \
+    && catkin_make -DCMAKE_BUILD_TYPE=Release
+
+# LIO-SAM
+COPY . /root/catkin_ws/src/
+RUN mkdir -p /root/catkin_ws/src \
+    && cd /root/catkin_ws/src \
+    #&& git clone https://github.com/jinkunw/LIO-SAM.git \
+    && cd .. \
+    && source /opt/ros/noetic/setup.bash \
     && catkin_make -DCMAKE_BUILD_TYPE=Release
 
 RUN echo "source /root/catkin_ws/devel/setup.bash" >> /root/.bashrc
--- src/imuPreintegration.cpp
+++ src/imuPreintegration.cpp
@@ -356,6 +356,7 @@ public:
             if (imuTime < currentCorrectionTime - delta_t)
             {
                 double dt = (lastImuT_opt < 0) ? (1.0 / 500.0) : (imuTime - lastImuT_opt);
+                dt = max(dt, 0.01);
                 imuIntegratorOpt_->integrateMeasurement(
                         gtsam::Vector3(thisImu->linear_acceleration.x, thisImu->linear_acceleration.y, thisImu->linear_acceleration.z),
                         gtsam::Vector3(thisImu->angular_velocity.x,    thisImu->angular_velocity.y,    thisImu->angular_velocity.z), dt);
@@ -424,6 +425,7 @@ public:
                 sensor_msgs::Imu *thisImu = &imuQueImu[i];
                 double imuTime = ROS_TIME(thisImu);
                 double dt = (lastImuQT < 0) ? (1.0 / 500.0) :(imuTime - lastImuQT);
+                dt = max(dt, 0.01);
 
                 imuIntegratorImu_->integrateMeasurement(gtsam::Vector3(thisImu->linear_acceleration.x, thisImu->linear_acceleration.y, thisImu->linear_acceleration.z),
                                                         gtsam::Vector3(thisImu->angular_velocity.x,    thisImu->angular_velocity.y,    thisImu->angular_velocity.z), dt);
@@ -470,6 +472,7 @@ public:
         double imuTime = ROS_TIME(&thisImu);
         double dt = (lastImuT_imu < 0) ? (1.0 / 500.0) : (imuTime - lastImuT_imu);
         lastImuT_imu = imuTime;
+                dt = max(dt, 0.01);
 
         // integrate this single imu message
         imuIntegratorImu_->integrateMeasurement(gtsam::Vector3(thisImu.linear_acceleration.x, thisImu.linear_acceleration.y, thisImu.linear_acceleration.z),
